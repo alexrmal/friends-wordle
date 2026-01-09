1495b692e8c826b405f9a163edffe2c690d0b659
Alex Maldonado <Alexmaldonado1667@gmail.com>
2026-01-08 22:16:34 -0800

    Added handleKeyPress function that reacts to user's inputs.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Friends Wordle loaded");
+const rows = document.querySelectorAll(".row");
+
+let currentRow = 0;
+let currentCol = 0;
+
+document.addEventListener("keydown", handleKeyPress);
+
+// This function runs everytime a key is press and handles them.
+function handleKeyPress(event) {
+    const key = event.key.toUpperCase();
+
+    // Listening for specific keyboard presses.
+    if (key === "BACKSPACE") {
+        removeLetter();
+    } else if (key === "ENTER") {
+        submitGuess();
+    } else if (key.length === 1 && key >= "A" && key <= "Z") {
+        addLetter(key);
+    }
+
+
+
+}
